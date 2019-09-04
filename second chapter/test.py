f561c17c841364f4e6150aa2eed78edbc740a825
Teacher-Guo <guoxian@nankai.edu.cn>
2019-09-04 21:38:34 +0800

    delete

--- second chapter/test.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-for i in range(200):
-    print((np.sin(i*0.01)+np.cos(i*0.01)-1)/(np.sin(i*0.01)*np.cos(i*0.01)+0.001))
-print(2*(np.sqrt(2)-1))
-x = np.sqrt(2)-1
-print(-7*np.power(x,6)-23*np.power(x,4)+8*np.power(x,3)-11*np.power(x,2)+3)
